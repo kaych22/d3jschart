db562535f945c376f3ac1219bf16de0b8faacfcf
Kishore C <kishorechandra18@gmail.com>
2021-01-17 00:00:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+// Data-Driven Documents
+// for d3 helper methods
+// console.log(d3); 
+
+const SAMPLE_DATA = [
+    { id: 'd1', value: 10, region: 'USA' },
+    { id: 'd2', value: 11, region: 'INDIA' },
+    { id: 'd3', value: 9, region: 'GERMANY' },
+    { id: 'd4', value: 7, region: 'POLAND' },
+    { id: 'd5', value: 4, region: 'BAVERIA' },
+];
+
+// for adding d3-scale library  
+const xScale = d3.scaleBand().domain(SAMPLE_DATA.map((dataPoint) => dataPoint.region)).rangeRound([0, 250]).padding(0,1);
+const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
+
+
+const container = d3.select('svg')
+    .classed('container', true)
+    // .style('border', '1px solid red');
+
+const bars = container
+    .selectAll('.bar')
+    .data(SAMPLE_DATA)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', xScale.bandwidth())
+    .attr('height', (data) => 200 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value));
+    // use attr instead of .style - static content
+
+    setTimeout(() => {
+        bars.data(SAMPLE_DATA.slice(0,2)).exit().remove();
+    }, 2000);
